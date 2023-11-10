61ecfc88a65c513e17887afe5c22dd6bae1a74e1
YukaNishihara <bibi0129@gmail.com>
2023-11-10 16:00:00 +0900

    Lesson3課題再提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,15 +2,17 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        int money = (int) tax(1500);
-        int total = money + 1500;
+        int price = 1500;
+        int tax = tax(price);
+        int total = tax + price;
 
-        System.out.println("1500円の商品の税込価格は" + total + "円（消費税は" + money + "円）です。");
+        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + tax + "円）です。");
 
     }
 
-    public static double tax(int num1) {
-        double result = num1 * 0.1;
+    public static int tax(int price) {
+        double rate = 0.1;
+        int result = (int)(price * rate);
         return result;
 
     }
