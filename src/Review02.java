6268ae8c4ebea491303e98b5aeb34846de16330e
YukaNishihara <bibi0129@gmail.com>
2023-11-11 15:01:32 +0900

    Lesson4課題

--- src/Review02.java
+++ src/Review02.java
@@ -3,7 +3,7 @@ public class Review02 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 101; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
